830e603d48f0f5dcf0ef38bf3fff63714df58159
Nguyễn Trung Nghĩa <nghiabgt@gmail.com>
2026-04-21 17:05:44 +0700

    feat: update admin menu layout with responsive container and layout component

--- app/components/Admin/Menu.vue
+++ app/components/Admin/Menu.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>admin menu</div>
+  <div class="flex h-full flex-col">
+    <ClientOnly>
+      <a-layout class="flex-1 overflow-y-auto"></a-layout>
+    </ClientOnly>
+  </div>
 </template>
