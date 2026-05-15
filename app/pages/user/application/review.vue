123b62acde2e98f8b298482aeb4dd1f0a35b9893
Nguyễn Trung Nghĩa <nghiabgt@gmail.com>
2026-05-15 15:10:17 +0700

    fix: adjust payment modal position for better visibility in application and review pages

--- app/pages/user/application/review.vue
+++ app/pages/user/application/review.vue
@@ -210,7 +210,7 @@
     </div>
   </a-modal>
 
-  <a-modal v-model:open="paymentVisible" title="Thanh toán phúc khảo" :width="960" :footer="null" @cancel="closePaymentModal">
+  <a-modal v-model:open="paymentVisible" title="Thanh toán phúc khảo" style="top: 0px" :width="960" :footer="null" @cancel="closePaymentModal">
     <div v-if="qrLoading" class="py-12 text-center">
       <a-spin size="large" />
     </div>
